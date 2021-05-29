6f0092fedb7debea0830168add63493461c13a2d
rendoruiz <rendo.ruiz@outlook.com>
2021-05-29 16:02:32 -0600

    home content complete

--- template3/js/main.js
+++ template3/js/main.js
@@ -1,6 +1,7 @@
-[...document.querySelectorAll('a')].forEach((link) => {
+[...document.querySelectorAll('.scroller')].forEach((link) => {
   if (link.dataset.scrollTo) {
     link.addEventListener('click', (e) => {
+      e.preventDefault();
       const scrollTarget = document.querySelector(`section.${e.target.dataset.scrollTo}`);
       scrollTarget.scrollIntoView({behavior: 'smooth'});
     })
