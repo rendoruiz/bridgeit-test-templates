d7e0ecf499df66d0557b2e002cc99a03d611af46
rendoruiz <rendo.ruiz@outlook.com>
2021-06-05 22:57:45 -0600

    carousel complete

--- template4/js/carousel.js
+++ template4/js/carousel.js
@@ -3,7 +3,11 @@ const selectedItemTransformStyle = 'scale(1.15)';
 let carouselList = document.querySelectorAll('.carousel > .carousel-item');
 let currentSelectedItem = document.querySelector(`.carousel-item.${selectedItemFlag}`);
 const initialIndex = [...carouselList].indexOf(currentSelectedItem);
+const carouselNavPrev = document.querySelector('.carousel-nav.prev');
+const carouselNavNext = document.querySelector('.carousel-nav.next');
 // console.log(initialIndex)
+
+
 
 // shift carousel items
 const transformCarouselItems = (selectedItem, carouselList) => {
@@ -22,6 +26,27 @@ const transformCarouselItems = (selectedItem, carouselList) => {
   })
 
   selectedItem.style.transform += ` ${selectedItemTransformStyle}`;
+
+  currentSelectedItem = selectedItem;
+}
+
+const getSelectedIndex = () => {
+  return [...carouselList].indexOf(currentSelectedItem);
+}
+console.log(getSelectedIndex());
+
+const displayCarouselNavigation = () => {
+  const selectedIndex = getSelectedIndex();
+  if (selectedIndex === 0) {
+    carouselNavPrev.style.display = 'none';
+  } else {
+    carouselNavPrev.style.display = 'inline-block';
+  }
+  if (selectedIndex === carouselList.length - 1) {
+    carouselNavNext.style.display = 'none';
+  } else {
+    carouselNavNext.style.display = 'inline-block';
+  }
 }
 
 // activates carousel transformations
@@ -29,7 +54,8 @@ const operateCarousel = (e => {
   const selectedItem = e.target;
 
   transformCarouselItems(selectedItem, carouselList);
-  currentSelectedItem = selectedItem;
+  // currentSelectedItem = selectedItem;
+  displayCarouselNavigation();
 });
 
 // initialize click event listeners
@@ -42,4 +68,21 @@ window.addEventListener('load', () => {
 // adjust transform values on resize
 window.addEventListener('resize', () => {
     transformCarouselItems(currentSelectedItem, carouselList);
+});
+
+carouselNavPrev.addEventListener('click', (e) => {
+  // console.log('prev');
+  if (getSelectedIndex() > 0) {
+    transformCarouselItems([...carouselList][getSelectedIndex() - 1], carouselList);
+    displayCarouselNavigation();
+  }
+  console.log(getSelectedIndex());
+});
+
+carouselNavNext.addEventListener('click', (e) => {
+  // console.log('next');
+  if (getSelectedIndex() < carouselList.length) {
+    transformCarouselItems([...carouselList][getSelectedIndex() + 1], carouselList);
+    displayCarouselNavigation();
+  }
 });
