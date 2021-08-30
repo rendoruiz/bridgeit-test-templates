73c6bc036e432e035c0ce820f36f6c7e695395d5
rendoruiz <rendo.ruiz@outlook.com>
2021-08-30 13:49:22 -0600

    fonts + header

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
@@ -1,9 +1,12 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from '../styles/AppHeader.module.css';
 
 const AppHeader = () => {
   return ( 
     <header className={styles.appHeader}>
-      HEADER
+      <div className={styles.logo}>ADS</div>
+      <FontAwesomeIcon icon={faBars} />
     </header>
   );
 }
