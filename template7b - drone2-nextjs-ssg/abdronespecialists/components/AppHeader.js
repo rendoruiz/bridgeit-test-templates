2fbd4b6bf86171a7a45c36b5a23031eaec5ed49e
rendoruiz <rendo.ruiz@outlook.com>
2021-08-30 16:23:29 -0600

    change anchor to next/link

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
@@ -1,6 +1,7 @@
 import { faBars, faBookmark, faCamera, faEnvelope, faHome, faToolbox, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
+import Link from 'next/link';
 import styles from '../styles/AppHeader.module.css';
 
 const AppHeader = () => { 
@@ -16,7 +17,9 @@ const AppHeader = () => {
 
   return ( 
     <header className={!isNavigationOpen ? styles.appHeader : styles.appHeaderNavActive}>
-      <div className={styles.logo}>ADS</div>
+      <Link href="/">
+        <a className={styles.logo}>ADS</a>
+      </Link>
       <FontAwesomeIcon 
         icon={faBars} 
         className={styles.menuButton} 
@@ -28,30 +31,42 @@ const AppHeader = () => {
         className={styles.menuPanel} 
         onClick={toggleNavigation}
       >
-        <a href="#" className={styles.navigationItem}>
-          <FontAwesomeIcon icon={faHome} className={styles.navigationIcon} />
-          Home
-        </a>
-        <a href="#" className={styles.navigationItem}>
-          <FontAwesomeIcon icon={faUserFriends} className={styles.navigationIcon} />
-          About
-        </a>
-        <a href="#" className={styles.navigationItem}>
-          <FontAwesomeIcon icon={faCamera} className={styles.navigationIcon} />
-          Services
-        </a>
-        <a href="#" className={styles.navigationItem}>
-          <FontAwesomeIcon icon={faBookmark} className={styles.navigationIcon} />
-          Portfolio
-        </a>
-        <a href="#" className={styles.navigationItem}>
-          <FontAwesomeIcon icon={faToolbox} className={styles.navigationIcon} />
-          Equipment
-        </a>
-        <a href="#" className={styles.navigationItem}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.navigationIcon} />
-          Contact Us
-        </a>
+        <Link href="/">
+          <a className={styles.navigationItem}>
+            <FontAwesomeIcon icon={faHome} className={styles.navigationIcon} />
+            Home
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navigationItem}>
+            <FontAwesomeIcon icon={faUserFriends} className={styles.navigationIcon} />
+            About
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navigationItem}>
+            <FontAwesomeIcon icon={faCamera} className={styles.navigationIcon} />
+            Services
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navigationItem}>
+            <FontAwesomeIcon icon={faBookmark} className={styles.navigationIcon} />
+            Portfolio
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navigationItem}>
+            <FontAwesomeIcon icon={faToolbox} className={styles.navigationIcon} />
+            Equipment
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navigationItem}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.navigationIcon} />
+            Contact Us
+          </a>
+        </Link>
       </nav>
     </header>
   );
