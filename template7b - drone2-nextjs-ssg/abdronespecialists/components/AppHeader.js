e8ad6634a0c7a0044286ff23fb64ce8bf0ad161c
rendoruiz <rendo.ruiz@outlook.com>
2021-08-30 14:56:16 -0600

    nav menu

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
@@ -1,12 +1,46 @@
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBookmark, faCamera, faEnvelope, faHome, faToolbox, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import styles from '../styles/AppHeader.module.css';
 
-const AppHeader = () => {
+const AppHeader = () => { 
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+
+  const toggleNavigation = () => {
+    setIsNavigationOpen(!isNavigationOpen);
+  }
+
   return ( 
     <header className={styles.appHeader}>
       <div className={styles.logo}>ADS</div>
-      <FontAwesomeIcon icon={faBars} />
+      <FontAwesomeIcon icon={faBars} className={styles.menuButton} onClick={toggleNavigation} />
+
+      <nav className={isNavigationOpen ? styles.menuPanelActive : styles.menuPanel}>
+        <a href="#" className={styles.navigationItem}>
+          <FontAwesomeIcon icon={faHome} className={styles.navigationIcon} />
+          Home
+        </a>
+        <a href="#" className={styles.navigationItem}>
+          <FontAwesomeIcon icon={faUserFriends} className={styles.navigationIcon} />
+          About
+        </a>
+        <a href="#" className={styles.navigationItem}>
+          <FontAwesomeIcon icon={faCamera} className={styles.navigationIcon} />
+          Services
+        </a>
+        <a href="#" className={styles.navigationItem}>
+          <FontAwesomeIcon icon={faBookmark} className={styles.navigationIcon} />
+          Portfolio
+        </a>
+        <a href="#" className={styles.navigationItem}>
+          <FontAwesomeIcon icon={faToolbox} className={styles.navigationIcon} />
+          Equipment
+        </a>
+        <a href="#" className={styles.navigationItem}>
+          <FontAwesomeIcon icon={faEnvelope} className={styles.navigationIcon} />
+          Contact Us
+        </a>
+      </nav>
     </header>
   );
 }
