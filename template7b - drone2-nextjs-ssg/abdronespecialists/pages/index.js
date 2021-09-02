6b6a7100b1e428265a40b16efccc52ed4cc25859
rendoruiz <rendo.ruiz@outlook.com>
2021-09-02 17:52:19 -0600

    contact complete

--- template7b - drone2-nextjs-ssg/abdronespecialists/pages/index.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/pages/index.js
@@ -484,19 +484,22 @@ export default function Home() {
       <div className={styles.sectionSeparator}></div>
 
       <section className={styles.sectionContact}>
-        <div className={styles.contactText}>
+        <div className={styles.contactNotice}>
           <Image 
             prefix="home-contact-bg"
             title="race car image"
             noLg
+            className={styles.contactImage}
           />
-          <div>
-            <span className={styles.heading3}>
-              <span>We're always ready</span>
-              <span> to take on new projects.</span>
-            </span>
-            <p>Keep scrolling to send us a quick message and we will get back to you within 24 hours. Thank you for choosing Alberta Drone Specialists.</p>
+          <div className={styles.contactContext}>
+            <div className={styles.contactText}>
+              <span className={styles.contactTextHeading}>
+                <span>We're always ready</span>
+                <span> to take on new projects.</span>
+              </span>
+              <p>Keep scrolling to send us a quick message and we will get back to you within 24 hours. Thank you for choosing Alberta Drone Specialists.</p>
             </div>
+          </div>
         </div>
         <Form className={styles.form} />
       </section>
