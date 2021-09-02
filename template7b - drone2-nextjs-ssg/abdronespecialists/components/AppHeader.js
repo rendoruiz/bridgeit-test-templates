02edadf25f20e02df12f25d92e1d8ee3d2154529
rendoruiz <rendo.ruiz@outlook.com>
2021-09-01 19:07:39 -0600

    adaptive header

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/AppHeader.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/AppHeader.module.css';
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const AppHeader = () => { 
   const [isNavigationOpen, setIsNavigationOpen] = useState(null);
@@ -64,10 +65,34 @@ const AppHeader = () => {
         <Link href="/">
           <a className={styles.navigationItem}>
             <FontAwesomeIcon icon={faEnvelope} className={styles.navigationIcon} />
-            Contact Us
+            Contact<span className={styles.mobile}> Us</span>
           </a>
         </Link>
       </nav>
+
+      <div className={styles.mediaLinks}>
+        <a 
+          className={styles.linkItem} 
+          href="https://www.youtube.com/channel/UCz8CmyohSEjXX0vGBF4YapQ"
+          title="youtube page"
+        >
+          <FontAwesomeIcon icon={faYoutube} className={styles.mediaIcon} />
+        </a>
+        <a 
+          className={styles.linkItem} 
+          href="https://www.facebook.com/Alberta-Drone-Specialists-108741628175831"
+          title="facebook page"
+        >
+          <FontAwesomeIcon icon={faFacebook} className={styles.mediaIcon} />
+        </a>
+        <a 
+          className={styles.linkItem} 
+          href="https://www.instagram.com/abdronespecialists/"
+          title="instagram page"
+        >
+          <FontAwesomeIcon icon={faInstagram} className={styles.mediaIcon} />
+        </a>
+      </div>
     </header>
   );
 }
