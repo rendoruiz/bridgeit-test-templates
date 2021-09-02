326510c75fd1deecc0d63f55eed61edff7bb8a88
rendoruiz <rendo.ruiz@outlook.com>
2021-09-01 20:06:37 -0600

    fontawesome fixes

--- template7b - drone2-nextjs-ssg/abdronespecialists/pages/index.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { useState } from 'react'
 import { Accordion } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
