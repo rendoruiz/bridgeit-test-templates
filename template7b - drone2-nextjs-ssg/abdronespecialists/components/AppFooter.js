f17b31b738009330381db61f05ddbb57c3b1221b
rendoruiz <rendo.ruiz@outlook.com>
2021-09-02 18:42:12 -0600

    footer complete

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/AppFooter.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/AppFooter.js
@@ -5,28 +5,30 @@ import styles from '../styles/AppFooter.module.css';
 const AppFooter = () => {
   return ( 
     <footer className={styles.appFooter}>
-      <div className={styles.contactSection}>
-        <span>Start a Project</span>
-        <p>We are ready for the challenge</p>
-        <a href="mailto:info@abdronespecialists.ca">info@abdronespecialists.ca</a>
-      </div>
-      <div className={styles.mediaSection}>
-        <span className={styles.mediaBlurb}>Let's get social.</span>
-        <div className={styles.externalLinks}>
-          <a href="https://www.youtube.com/channel/UCz8CmyohSEjXX0vGBF4YapQ" target="_blank" rel="noreferrer" title="YouTube channel link">
-            <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
-          </a>
-          <a href="https://www.facebook.com/Alberta-Drone-Specialists-108741628175831" target="_blank" rel="noreferrer" title="Facebook page link">
-            <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
-          </a>
-          <a href="https://www.instagram.com/abdronespecialists/" target="_blank" rel="noreferrer" title="Instagram page link">
-            <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
-          </a>
+      <div className={styles.content}>
+        <div className={styles.contactSection}>
+          <span>Start a Project</span>
+          <p>We are ready for the challenge</p>
+          <a href="mailto:info@abdronespecialists.ca">info@abdronespecialists.ca</a>
         </div>
-      </div>
-      <div className={styles.creditSection}>
-        <span className={styles.copyright}>&copy; Alberta Drone Specialicts Ltd. 2021 All Rights Reserved</span>
-        <span className={styles.credits}>Designed & Developed by <a href="https://bridgeittech.ca/" target="_blank" rel="noreferrer" title="developer link">BridgeIT Tech</a></span>
+        <div className={styles.mediaSection}>
+          <span className={styles.mediaBlurb}>Let's get social.</span>
+          <div className={styles.externalLinks}>
+            <a href="https://www.youtube.com/channel/UCz8CmyohSEjXX0vGBF4YapQ" target="_blank" rel="noreferrer" title="YouTube channel link">
+              <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
+            </a>
+            <a href="https://www.facebook.com/Alberta-Drone-Specialists-108741628175831" target="_blank" rel="noreferrer" title="Facebook page link">
+              <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
+            </a>
+            <a href="https://www.instagram.com/abdronespecialists/" target="_blank" rel="noreferrer" title="Instagram page link">
+              <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+            </a>
+          </div>
+        </div>
+        <div className={styles.creditSection}>
+          <span>&copy; Alberta Drone Specialists Ltd. 2021 All Rights Reserved</span>
+          <span>Designed & Developed by <a href="https://bridgeittech.ca/" target="_blank" rel="noreferrer" title="developer link">BridgeIT Tech</a></span>
+        </div>
       </div>
     </footer>
   );
