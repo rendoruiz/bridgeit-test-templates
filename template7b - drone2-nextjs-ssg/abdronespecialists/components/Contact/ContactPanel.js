93d9f9a40e3a4c004d8a8109243193bf1b4c195c
rendoruiz <rendo.ruiz@outlook.com>
2021-09-03 19:00:23 -0600

    rename

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Contact/ContactPanel.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Contact/ContactPanel.js
@@ -5,21 +5,21 @@ import styles from '../../styles/Contact/ContactPanel.module.css'
 
 const ContactPanel = () => {
   return ( 
-    <section className={styles.sectionContact}>
-      <div className={styles.contactNotice}>
+    <section className={styles.contact}>
+      <div className={styles.sideContent}>
         <Image 
           prefix="home-contact-bg"
           title="race car image"
           noLg
-          className={styles.contactImage}
+          className={styles.background}
         />
-        <div className={styles.contactContext}>
-          <div className={styles.contactText}>
-            <span className={styles.contactTextHeading}>
+        <div className={styles.textContainer}>
+          <div className={styles.sideText}>
+            <span className={styles.heading}>
               <span>We're always ready</span>
               <span> to take on new projects.</span>
             </span>
-            <p>Keep scrolling to send us a quick message and we will get back to you within 24 hours. Thank you for choosing Alberta Drone Specialists.</p>
+            <p className={styles.subtitle}>Keep scrolling to send us a quick message and we will get back to you within 24 hours. Thank you for choosing Alberta Drone Specialists.</p>
           </div>
         </div>
       </div>
