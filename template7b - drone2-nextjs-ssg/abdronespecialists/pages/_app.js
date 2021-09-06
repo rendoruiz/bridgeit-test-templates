c1f46567bceb58c16b865e37bc3864ac8a9d07cb
rendoruiz <rendo.ruiz@outlook.com>
2021-09-06 14:38:08 -0600

    fontawesome nextjs fix

--- template7b - drone2-nextjs-ssg/abdronespecialists/pages/_app.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/pages/_app.js
@@ -2,6 +2,10 @@ import "slick-carousel/slick/slick.css";
 import '../styles/bootstrap_accordion.css';
 import '../styles/_globals.css';
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
