8d41337e7a0702a5c10204176a5efd899b3b4768
rendoruiz <rendo.ruiz@outlook.com>
2021-09-07 14:28:17 -0600

    menu complete

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Equipment/DroneMenu.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Equipment/DroneMenu.js
@@ -24,7 +24,7 @@ const DroneMenu = ({ className }) => {
     <div className={classNames(styles.container, className)}>
       <button className={classNames(styles.toggle, isMenuOpen ? styles.active : null)} onClick={toggleMenu}>Menu</button>
 
-      <div className={classNames(styles.menu, isMenuOpen ? styles.active : null)}>
+      <div className={classNames(styles.menu, isMenuOpen ? styles.active : null)} onClick={toggleMenu}>
         <Link href="/equipment?id=0" shallow>
           <a className={classNames(styles.menuItem, setActiveMenuItem('0'))}>DJI Inspire 2</a>
         </Link>
@@ -41,12 +41,6 @@ const DroneMenu = ({ className }) => {
           <a className={classNames(styles.menuItem, setActiveMenuItem('4'))}>Indoor FPV</a>
         </Link>
       </div>
-
-      {/* <button onClick={() => router.push('/equipment?id=0', undefined, { shallow: true })}>1</button>
-      <button onClick={() => router.push('/equipment?id=1', undefined, { shallow: true })}>2</button>
-      <button onClick={() => router.push('/equipment?id=2', undefined, { shallow: true })}>3</button>
-      <button onClick={() => router.push('/equipment?id=3', undefined, { shallow: true })}>4</button>
-      <button onClick={() => router.push('/equipment?id=4', undefined, { shallow: true })}>5</button> */}
     </div>
   );
 }
