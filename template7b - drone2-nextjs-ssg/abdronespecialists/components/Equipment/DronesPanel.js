cce96bf6902e4d051cec17b784b714e0b92d560d
rendoruiz <rendo.ruiz@outlook.com>
2021-09-06 18:42:07 -0600

    decouple dronepanel

---
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Equipment/DronesPanel.js
@@ -0,0 +1,79 @@
+import classNames from "classnames";
+
+import Carousel from '../../components/Carousel'
+import DroneCard from '../../components/DroneCard'
+
+import styles from '../../styles/Equipment/DronesPanel.module.css'
+
+const DronesPanel = ({ carouselClassName, cardClassName, noArrows, infinite, carouselOnly }) => {
+  const droneList = [
+    {
+      imagePrefix: 'equipment-dji-inspire-2',
+      imageTitle: 'DJI Inspire 2 image',
+      heading: 'DJI Inspire 2',
+      bodyText: 'Image quality, power and intelligence to meet the needs of professional filmmakers and enterprises around the globe.',
+      href: '/'
+    },
+    {
+      imagePrefix: 'equipment-zenmuse-x7',
+      imageTitle: 'Zenmuse X7 image',
+      heading: 'Zenmuse X7',
+      bodyText: 'The Zenmuse X7 is a compact Super 35 camera with an integrated gimbal made for high-end filmmaking.',
+      href: '/',
+      fileExtension: 'png'
+    },
+    {
+      imagePrefix: 'equipment-indemnis-nexus',
+      imageTitle: 'Indemnis Nexus image',
+      heading: 'Indemnis Nexus',
+      bodyText: 'The Nexus by Indemnis is the first UAV recovery system to be proven reliable in any failure situation.',
+      href: '/',
+      fileExtension: 'png'
+    },
+    {
+      imagePrefix: 'equipment-outdoor-fpv',
+      imageTitle: 'Outdoor FPV image',
+      heading: 'Outdoor FPV',
+      bodyText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci molestiae quia suscipit ratione repellendus facilis.',
+      href: '/'
+    },
+    {
+      imagePrefix: 'equipment-indoor-fpv',
+      imageTitle: 'Indoor FPV image',
+      heading: 'Indoor FPV',
+      bodyText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci molestiae quia suscipit ratione repellendus facilis.',
+      href: '/'
+    }
+  ];
+
+  const getDrones = (keyPrefix) => {
+    return droneList.map((drone, index) => (
+      <DroneCard 
+        key={keyPrefix + index}
+        imagePrefix={drone.imagePrefix}
+        imageTitle={drone.imageTitle}
+        heading={drone.heading}
+        body={drone.bodyText}
+        href={drone.href}
+        fileExtension={drone.fileExtension}
+      />
+    ));
+  }
+
+  return <>
+    <Carousel 
+      className={classNames(styles.carousel, carouselClassName)} 
+      noArrows={noArrows}
+      infinite={infinite}
+    >
+      { getDrones('carousel') }
+    </Carousel>
+    { !carouselOnly && (
+      <div className={classNames(styles.cards, cardClassName)}>
+        { getDrones('card') }
+      </div>
+    )}
+  </>
+}
+ 
+export default DronesPanel;
--- template7b - drone2-nextjs-ssg/abdronespecialists/pages/index.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/pages/index.js
@@ -9,15 +9,14 @@ import MainLayout from '../components/layouts/MainLayout'
 import Image from '../components/Image'
 import Video from '../components/Video'
 import Carousel from '../components/Carousel'
-import DroneCard from '../components/DroneCard'
 import VideoModal from '../components/VideoModal'
 import Partners from '../components/About/Partners'
-import DroneList from '../components/DroneList'
 import Highlights from '../components/About/Highlights'
 import Divider from '../components/Divider'
 import ContactPanel from '../components/Contact/ContactPanel'
 
 import styles from '../styles/Home.module.css'
+import DronesPanel from '../components/Equipment/DronesPanel'
 
 export default function Home() {
   return (
@@ -75,55 +74,16 @@ export default function Home() {
         </div>
       </section>
 
+      
       <section className={styles.sectionEquipment}>
         <div className={styles.equipmentContent}>
           <h2 className={styles.heading2}>Our Equipment</h2>
-          <Carousel 
-            className={styles.carousel} 
-            noArrows
-            infinite
-          >
-            <DroneCard 
-              imagePrefix="equipment-dji-inspire-2"
-              imageTitle="DJI Inspire 2 image"
-              heading="DJI Inspire 2"
-              body="Image quality, power and intelligence to meet the needs of professional filmmakers and enterprises around the globe."
-              href="/"
-            />
-            <DroneCard 
-              imagePrefix="equipment-zenmuse-x7"
-              imageTitle="Zenmuse X7 image"
-              heading="Zenmuse X7"
-              body="The Zenmuse X7 is a compact Super 35 camera with an integrated gimbal made for high-end filmmaking."
-              href="/"
-              fileExtension="png"
-            />
-            <DroneCard 
-              imagePrefix="equipment-indemnis-nexus"
-              imageTitle="Indemnis Nexus image"
-              heading="Indemnis Nexus"
-              body="The Nexus by Indemnis is the first UAV recovery system to be proven reliable in any failure situation."
-              href="/"
-              fileExtension="png"
-            />
-            <DroneCard 
-              imagePrefix="equipment-outdoor-fpv"
-              imageTitle="Outdoor FPV image"
-              heading="Outdoor FPV"
-              body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci molestiae quia suscipit ratione repellendus facilis."
-              href="/"
-            />
-            <DroneCard 
-              imagePrefix="equipment-indoor-fpv"
-              imageTitle="Indoor FPV image"
-              heading="Indoor FPV"
-              body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci molestiae quia suscipit ratione repellendus facilis."
-              href="/"
-            />  
-          </Carousel>
-          <div className={styles.droneCards}>
-            <DroneList />
-          </div>
+          <DronesPanel 
+            carouselClassName={styles.equipmentCarousel}
+            cardClassName={styles.equipmentCards}
+            noArrows 
+            infinite 
+          />
         </div>
       </section>
 
