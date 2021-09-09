61d0d50bae8bfdb23e001989b295c4d3e9393e1f
rendoruiz <rendo.ruiz@outlook.com>
2021-09-09 16:03:58 -0600

    update services

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Services/ServiceCard.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Services/ServiceCard.js
@@ -1,10 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Button from '../../components/Button'
 import classNames from 'classnames';
-import Link from 'next/link'
 
 import styles from '../../styles/Services/ServiceCard.module.css'
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const ServiceCard = ({ className, title, body, icon, svgIcon, routeTo }) => {
   const getIcon = () => {
@@ -23,21 +21,21 @@ const ServiceCard = ({ className, title, body, icon, svgIcon, routeTo }) => {
       ) 
     }
   }
+
   return ( 
-    <Link href={routeTo ?? "/404"}>
-      <a className={classNames(styles.card, className)}>
-        { getIcon() }
-        <h3 className={styles.heading}>{ title }</h3>
-        <p className={styles.body}>{ body }</p>
-        <span className={styles.linkText}>
-          {"Explore " + title}
-          <FontAwesomeIcon 
-            icon={faAngleRight} 
-            className={styles.linkTextIcon}
-          />
-        </span>
-      </a>
-    </Link>
+    <div className={classNames(styles.card, className)}>
+      { getIcon() }
+      <h3 className={styles.heading}>{ title }</h3>
+      <p className={styles.body}>{ body }</p>
+
+      <Button 
+        className={styles.link}
+        iconClassName={styles.linkIcon}
+        text={"Explore " + title}
+        title={title + "page link"}
+        href={routeTo}
+      />
+    </div>
   );
 }
  
