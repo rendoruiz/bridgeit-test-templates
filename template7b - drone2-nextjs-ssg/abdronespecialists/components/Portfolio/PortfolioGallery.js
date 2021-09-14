1cf1df9c426b17fce9c4088bf22da2a19637571a
rendoruiz <rendo.ruiz@outlook.com>
2021-09-14 11:36:21 -0600

    image switching

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Portfolio/PortfolioGallery.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Portfolio/PortfolioGallery.js
@@ -5,44 +5,48 @@ import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 const PortfolioGallery = ({ className, data }) => {
-  const [primaryMedia, setPrimaryMedia] = useState(null);
-  const [secondaryMediaA, setSecondaryMediaA] = useState(null);
-  const [secondaryMediaB, setSecondaryMediaB] = useState(null);
-  const [secondaryMediaC, setSecondaryMediaC] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [images, setImages] = useState({
+    primary: 'https://cdn.pixabay.com/photo/2021/07/19/15/47/lily-6478427_1280.jpg',
+    children: [
+      'https://cdn.pixabay.com/photo/2020/01/01/18/13/train-4734126_1280.jpg',
+      'https://cdn.pixabay.com/photo/2021/08/07/08/49/building-6528075_1280.jpg',
+      'https://cdn.pixabay.com/photo/2020/07/21/16/24/landscape-5426755_1280.jpg'
+    ]
+  });
 
-  useEffect(() => {
-    if (data) {
-      console.log(data)
-      setPrimaryMedia(data.primaryMedia.imagePrefix);
-      setSecondaryMediaA(data.secondaryMediaList[0].imagePrefix);
-      setSecondaryMediaB(data.secondaryMediaList[1].imagePrefix);
-      setSecondaryMediaC(data.secondaryMediaList[2].imagePrefix);
-      setIsLoading(false)
-    }
-  }, [data])
 
-  return isLoading ? null : ( 
+  const switchImage = (fromIndex) => {
+    const copy = {...images};
+    copy.primary = images.children[fromIndex];
+    copy.children[fromIndex] = images.primary;
+    setImages(copy);
+  }
+
+  return ( 
     <div className={classNames(styles.content, className)}>
       <div className={classNames(styles.image, styles.primary)}>
-        <Image 
+        {/* <Image 
           prefix={primaryMedia} noMd noLg
-        />
+        /> */}
+        <img src={images.primary} alt="" />
       </div>
-      <div className={classNames(styles.image)}>
-        <Image 
+      <div className={classNames(styles.image)} onClick={() => switchImage(0)}>
+        {/* <Image 
           prefix={primaryMedia} noMd noLg
-        />
+        /> */}
+        <img src={images.children[0]} alt="" />
       </div>
-      <div className={classNames(styles.image)}>
-        <Image 
+      <div className={classNames(styles.image)} onClick={() => switchImage(1)}>
+        {/* <Image 
           prefix={primaryMedia} noMd noLg
-        />
+        /> */}
+        <img src={images.children[1]} alt="" />
       </div>
-      <div className={classNames(styles.image)}>
-        <Image 
+      <div className={classNames(styles.image)} onClick={() => switchImage(2)}>
+        {/* <Image 
           prefix={primaryMedia} noMd noLg
-        />
+        /> */}
+        <img src={images.children[2]} alt="" />
       </div>
     </div>
   ); 
