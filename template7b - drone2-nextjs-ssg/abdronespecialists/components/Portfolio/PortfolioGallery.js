3e2d11bf3a787e74c84dcd5fe74e53993f7c9bfb
rendoruiz <rendo.ruiz@outlook.com>
2021-09-14 12:50:04 -0600

    testing

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Portfolio/PortfolioGallery.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Portfolio/PortfolioGallery.js
@@ -1,8 +1,9 @@
 import Image from '../Image';
 
 import styles from '../../styles/Portfolio/PortfolioGallery.module.css'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
+import { useContainerDimensions } from '../../hooks/useContainerDimensions';
 
 const PortfolioGallery = ({ className, data }) => {
   const [images, setImages] = useState({
@@ -13,23 +14,99 @@ const PortfolioGallery = ({ className, data }) => {
       'https://cdn.pixabay.com/photo/2020/07/21/16/24/landscape-5426755_1280.jpg'
     ]
   });
+  const [isSwitching, setIsSwitching] = useState(false);
+
+
+  const primaryRef = useRef();
+  const secondaryRef = Array(3).fill(useRef());
+  const { width: primaryWidth, height: primaryHeight } = useContainerDimensions(primaryRef);
+  const { width: secondaryWidth, height: secondaryHeight } = useContainerDimensions(secondaryRef[0]);
+  
+  console.log(primaryWidth, primaryHeight);
+  console.log(secondaryWidth, secondaryHeight);
 
   const switchImage = (fromIndex) => {
-    const copy = {...images};
-    copy.primary = images.children[fromIndex];
-    copy.children[fromIndex] = images.primary;
-    setImages(copy);
+    if (!isSwitching) {
+      // init status
+      setIsSwitching(true);
+
+      // animate switching
+      animateSwitch(fromIndex);
+
+      // switch images
+      const copy = {...images};
+      copy.primary = images.children[fromIndex];
+      copy.children[fromIndex] = images.primary;
+      setImages(copy);
+      
+      // update status
+      setIsSwitching(false);
+    }
+  }
+
+  const animateSwitch = (itemIndex) => {
+    // animate focused item bottom swap
+    primaryRef.current.style = {
+      transition: 'width 100ms ease-out, height 100ms ease-out, transform 200ms ease-out',
+      width: secondaryWidth + 'px',
+      height: secondaryHeight + 'px',
+      transform: `translate(calc(calc(100% + var(--grid-gap)) * ${itemIndex}), calc(200% + var(--grid-gap))) scale(1.05)`
+    }
+    secondaryRef[itemIndex].current.style = {
+      position: 'absolute',
+      transition: 'width 100ms ease-out, height 100ms ease-out, transform 200ms ease-out',
+      width: primaryWidth + 'px',
+      height: primaryHeight + 'px',
+      transform: `translate(calc(calc(-100% - var(--grid-gap)) / 3 * ${itemIndex}), calc(-100% - var(--grid-gap))) scale(1.05)`
+    }
+
+    // post-bobbing animation 
+    setTimeout(() => {
+      primaryRef.current.style = {
+        transition: 'transform 100ms ease-out',
+        transform: `translate(calc(calc(100% + var(--grid-gap)) * ${itemIndex}), calc(200% + var(--grid-gap))) scale(1.0)`
+      }
+      secondaryRef[itemIndex].current.style = {
+        transition: 'transform 100ms ease-out',
+        transform: `translate(calc(calc(-100% - var(--grid-gap)) / 3 * ${itemIndex}), calc(-100% - var(--grid-gap))) scale(1.0)`
+      }
+
+      // reset & set
+      setTimeout(() => {
+        // reset property changes & animation
+        primaryRef.current.style = {
+          transition: 'none',
+          width: '100%',
+          height: '100%',
+          transform: 'unset'
+        }
+        secondaryRef[itemIndex].current.style = {
+          position: 'static',
+          transition: 'none',
+          width: '100%',
+          height: '100%',
+          transform: 'unset'
+        }
+      }, 100);
+    }, 200);
   }
 
   return ( 
     <div className={classNames(styles.content, className)}>
-      <div className={classNames(styles.image, styles.primary)}>
+      <div 
+        className={classNames(styles.image, styles.primary)} 
+        ref={primaryRef}
+      >
         {/* <Image 
           prefix={primaryMedia} noMd noLg
         /> */}
         <img src={images.primary} alt="" />
       </div>
-      <div className={classNames(styles.image)} onClick={() => switchImage(0)}>
+      <div 
+        className={classNames(styles.image)} 
+        onClick={() => switchImage(0)} 
+        ref={secondaryRef[0]}
+      >
         {/* <Image 
           prefix={primaryMedia} noMd noLg
         /> */}
