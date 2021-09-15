9b0678ad51b10e795e5394294cffe729461f179f
rendoruiz <rendo.ruiz@outlook.com>
2021-09-15 14:43:43 -0600

    form request

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Contact/Form.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Contact/Form.js
@@ -2,12 +2,45 @@ import { faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import FormInput from "./FormInput"
 import Button from "../Button";
+import api from "./api";
 
 import styles from '../../styles/Contact/Form.module.css';
 
 const Form = ({ className }) => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const formContent = {
+      name: event.target.name.value,
+      sender: event.target.email.value,
+      subject: event.target.subject.value,
+      body: event.target.message.value
+    }
+    api.post('sendemail', formContent)
+    .then((response) => {
+      console.log("response:", response)
+      console.log("response.data:", response.data)
+      console.log("response.status:", response.status)
+      console.log("response.headers:", response.headers)
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log("error.response.data:", error.response.data);
+        console.log("error.response.status:", error.response.status);
+        console.log("error.response.headers:", error.response.headers);
+      } else if (error.request) {
+        console.log("error.request:", error.request);
+      } else {
+        console.log("error", error.message);
+      }
+    })
+  }
+
   return ( 
-    <form className={styles.form + (!className ? '' : ` ${className}`)}>
+    <form 
+      className={styles.form + (!className ? '' : ` ${className}`)}
+      onSubmit={sendEmail}
+    >
       <h2 className={styles.heading}>Send us a message.</h2>
       <FormInput 
         label="First & Last Name"
