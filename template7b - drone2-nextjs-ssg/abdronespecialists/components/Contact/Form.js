c94d1b76a961e22b5ffd161372f4dea59090261b
rendoruiz <rendo.ruiz@outlook.com>
2021-09-15 17:34:50 -0600

    success modal

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Contact/Form.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Contact/Form.js
@@ -5,35 +5,52 @@ import Button from "../Button";
 import api from "./api";
 
 import styles from '../../styles/Contact/Form.module.css';
+import { useState } from "react";
+import ModalSuccess from "./ModalSuccess";
 
 const Form = ({ className }) => {
+  const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+  const [serverMessage, setServerMessage] = useState(null);
+
+  const toggleSuccessModal = () => {
+    setIsSuccessModalOpen(!isSuccessModalOpen);
+  }
+  const toggleErrorModal = (error) => {
+    setIsErrorModalOpen(!isErrorModalOpen);
+  }
+
   const sendEmail = (e) => {
     e.preventDefault();
+    toggleSuccessModal();
 
-    const formContent = {
-      name: event.target.name.value,
-      sender: event.target.email.value,
-      subject: event.target.subject.value,
-      body: event.target.message.value
-    }
-    api.post('sendemail', formContent)
-    .then((response) => {
-      console.log("response:", response)
-      console.log("response.data:", response.data)
-      console.log("response.status:", response.status)
-      console.log("response.headers:", response.headers)
-    })
-    .catch((error) => {
-      if (error.response) {
-        console.log("error.response.data:", error.response.data);
-        console.log("error.response.status:", error.response.status);
-        console.log("error.response.headers:", error.response.headers);
-      } else if (error.request) {
-        console.log("error.request:", error.request);
-      } else {
-        console.log("error", error.message);
-      }
-    })
+    // const formContent = {
+    //   name: event.target.name.value,
+    //   sender: event.target.email.value,
+    //   subject: event.target.subject.value,
+    //   body: event.target.message.value
+    // }
+    // api.post('sendemail', formContent)
+    // .then((response) => {
+    //   if (request.status === 200) {
+    //     console.log("response:", response)
+    //     console.log("response.data:", response.data)
+    //     console.log("response.status:", response.status)
+    //     console.log("response.headers:", response.headers)
+    //     toggleSuccessModal();
+    //   }
+    // })
+    // .catch((error) => {
+    //   if (error.response) {
+    //     console.log("error.response.data:", error.response.data);
+    //     console.log("error.response.status:", error.response.status);
+    //     console.log("error.response.headers:", error.response.headers);
+    //   } else if (error.request) {
+    //     console.log("error.request:", error.request);
+    //   } else {
+    //     console.log("error", error.message);
+    //   }
+    // })
   }
 
   return ( 
@@ -72,6 +89,7 @@ const Form = ({ className }) => {
         text="Send" 
         className={styles.button} 
       />
+      <ModalSuccess status={isSuccessModalOpen} toggle={toggleSuccessModal} />
     </form>
   );
 }
--- template7b - drone2-nextjs-ssg/abdronespecialists/components/CustomModal.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/CustomModal.js
@@ -1,16 +1,17 @@
+import classNames from 'classnames';
 import Modal from 'react-modal';
 import styles from '../styles/Modal.module.css';
 
 Modal.setAppElement('#__next');
 
-const CustomModal = ({ children, title, isOpen, onRequestClose }) => {
+const CustomModal = ({ children, title, isOpen, onRequestClose, className, overlayClassName }) => {
   return !children ? null : ( 
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel={title ?? "Modal"}
-      className={styles.modal}
-      overlayClassName={styles.overlay}
+      className={classNames(styles.modal, className)}
+      overlayClassName={classNames(styles.overlay, overlayClassName)}
       closeTimeoutMS={200}
     >
       { children }
