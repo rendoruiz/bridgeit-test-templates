afb7e16040913d82991980b63f85d3da2831d2bb
rendoruiz <rendo.ruiz@outlook.com>
2021-09-15 18:52:11 -0600

    contact complete

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/Button.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/Button.js
@@ -1,10 +1,11 @@
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 import styles from '../styles/Button.module.css';
 
-const Button = ({ text, noIcon = false, onClick, className, appendClassName, iconClassName, title, href }) => {
+const Button = ({ text, noIcon = false, onClick, className, appendClassName, iconClassName, title, href, disabled }) => {
   const addIcon = () => {
     return noIcon ? null :
       <FontAwesomeIcon 
@@ -28,6 +29,7 @@ const Button = ({ text, noIcon = false, onClick, className, appendClassName, ico
         onClick={onClick}
           className={!appendClassName ? (className ?? styles.button) : (styles.button + (!className ? '' : ` ${className}`))}
         title={title ?? 'button'}
+        disabled={disabled}
       >
         { text }
         { addIcon() }
