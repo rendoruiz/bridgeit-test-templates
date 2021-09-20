a34610894a0ddac8810699934386054d88d015bb
rendoruiz <rendo.ruiz@outlook.com>
2021-09-19 19:41:33 -0600

    favicon

--- template7b - drone2-nextjs-ssg/abdronespecialists/components/MainLayout.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/components/MainLayout.js
@@ -14,6 +14,10 @@ const MainLayout = ({ children }) => {
         {/* <link rel="preload" href="https://use.typekit.net/smp6txj.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
         <noscript><link rel="stylesheet" href="https://use.typekit.net/smp6txj.css" /></noscript> */}
         <link rel="stylesheet" href="https://use.typekit.net/smp6txj.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <AppHeader />
       { children }
