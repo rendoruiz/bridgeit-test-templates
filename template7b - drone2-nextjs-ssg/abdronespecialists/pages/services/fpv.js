640b5dc37d83102bc72eb68c020fdb47105b8d8d
rendoruiz <rendo.ruiz@outlook.com>
2021-09-19 18:36:17 -0600

    Update fpv.js

--- template7b - drone2-nextjs-ssg/abdronespecialists/pages/services/fpv.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/pages/services/fpv.js
@@ -24,7 +24,7 @@ const FpvServicePage = () => {
       contentClassName={styles.content}
     >
       <Head>
-        <title>Services | FPV| Alberta Drone Specialists</title>
+        <title>Services | FPV | Alberta Drone Specialists</title>
         <meta name="description" content="Our experienced drone pilots provide exceptional indoor and outdoor FPV drone services that capture cinematic shots like never before."></meta>
         <meta property="og:title" content="Services | FPV| Alberta Drone Specialists"></meta>
         <meta property="og:description" content="Our experienced drone pilots provide exceptional indoor and outdoor FPV drone services that capture cinematic shots like never before."></meta>
