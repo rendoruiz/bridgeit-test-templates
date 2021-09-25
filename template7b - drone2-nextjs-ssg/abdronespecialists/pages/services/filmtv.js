f2afb1030c56808e38aea8ad7c6542e478e4936b
rendoruiz <rendo.ruiz@outlook.com>
2021-09-24 19:01:29 -0600

    film&tv complete

--- template7b - drone2-nextjs-ssg/abdronespecialists/pages/services/filmtv.js
+++ template7b - drone2-nextjs-ssg/abdronespecialists/pages/services/filmtv.js
@@ -5,8 +5,10 @@ import ServicesLayout from "../../components/Services/ServicesLayout";
 import ServiceDetailCard from '../../components/Services/ServiceDetailCard';
 import Carousel from '../../components/Carousel'
 import VideoModal from '../../components/VideoModal';
+import Image from '../../components/Image';
 
 import styles from '../../styles/Services/ServicesDetail.module.css';
+import classNames from 'classnames';
 
 const FilmTvServicePage = () => {
   const data = {
@@ -15,7 +17,29 @@ const FilmTvServicePage = () => {
     summaryText: 'Professional drone crew services for films, television, and commercials. Our team of drone operators, drone camera operators, and drone technicians have experience working with a wide range of productions from integrating with main units on large productions to splinter units on lower budget productions.',
     detailText: 'Our team of drone operators, drone camera operators, and drone technicians have experience working with a wide range of productions from integrating with main units on large productions to splinter units on lower budget productions.',
     routeTo: '/services/filmtv',
-    icon: faPlayCircle
+    icon: faPlayCircle,
+    primaryMedia: {
+      prefix: 'services-filmtv-1',
+      title: 'film & tv service photo',
+    },
+    secondaryMedia: [
+      {
+        prefix: 'services-filmtv-2',
+        title: 'film & tv service photo'
+      },
+      {
+        prefix: 'services-filmtv-3',
+        title: 'film & tv service photo'
+      },
+      {
+        prefix: 'services-filmtv-4',
+        title: 'film & tv service photo'
+      },
+      {
+        prefix: 'services-filmtv-5',
+        title: 'film & tv service photo'
+      }
+    ]
   }
 
   return ( 
@@ -32,10 +56,9 @@ const FilmTvServicePage = () => {
         <meta property="og:description" content="Our experienced drone pilots provide professional drone services for film, television and commercials."></meta>
       </Head>
       
-      <VideoModal 
-        imagePrefix="highlights-pv1"
-        title="highlight video"
-        youtubeEmbedId="ifO6DECu6-k"
+      <img 
+        src={'/images/' + data.primaryMedia.prefix + '-md.jpg'}
+        alt={data.primaryMedia.title}
         className={styles.mediaPrimary}
       />
       
@@ -48,65 +71,27 @@ const FilmTvServicePage = () => {
       />
 
       <div className={styles.mediaSecondaries}>
-        <VideoModal 
-          imagePrefix="highlights-pv1"
-          title="highlight video"
-          youtubeEmbedId="ifO6DECu6-k"
-          className={styles.mediaSecondary}
-        />
-        <VideoModal 
-          imagePrefix="highlights-pv1"
-          title="highlight video"
-          youtubeEmbedId="ifO6DECu6-k"
-          className={styles.mediaSecondary}
-        />
-        <VideoModal 
-          imagePrefix="highlights-pv1"
-          title="highlight video"
-          youtubeEmbedId="ifO6DECu6-k"
-          className={styles.mediaSecondary}
-        />
-        <VideoModal 
-          imagePrefix="highlights-pv1"
-          title="highlight video"
-          youtubeEmbedId="ifO6DECu6-k"
-          className={styles.mediaSecondary}
-        />
+        {[...data.secondaryMedia].map((media, index) => (
+          <img 
+            key={'service-filmtv-carousel-' + index}
+            src={'/images/' + media.prefix + '-md.jpg'}
+            alt={media.title}
+            className={styles.mediaSecondary}
+          />
+        ))}
       </div>
 
       <div className={styles.carouselContainer}>
         <h2 className={styles.heading}>Our Work</h2>
         <Carousel noArrows>
-          <VideoModal 
-            imagePrefix="highlights-pv1"
-            title="highlight video"
-            youtubeEmbedId="ifO6DECu6-k"
-            className={styles.carouselItem}
-          />
-          <VideoModal 
-            imagePrefix="highlights-pv1"
-            title="highlight video"
-            youtubeEmbedId="ifO6DECu6-k"
-            className={styles.carouselItem}
-          />
-          <VideoModal 
-            imagePrefix="highlights-pv1"
-            title="highlight video"
-            youtubeEmbedId="ifO6DECu6-k"
-            className={styles.carouselItem}
-          />
-          <VideoModal 
-            imagePrefix="highlights-pv1"
-            title="highlight video"
-            youtubeEmbedId="ifO6DECu6-k"
-            className={styles.carouselItem}
-          />
-          <VideoModal 
-            imagePrefix="highlights-pv1"
-            title="highlight video"
-            youtubeEmbedId="ifO6DECu6-k"
-            className={styles.carouselItem}
-          />
+          {[data.primaryMedia, ...data.secondaryMedia].map((media, index) => (
+            <img 
+              key={'service-filmtv-carousel-' + index}
+              src={'/images/' + media.prefix + '-sm.jpg'}
+              alt={media.title}
+              className={styles.carouselImage}
+            />
+          ))}
         </Carousel>
       </div>
     </ServicesLayout>
